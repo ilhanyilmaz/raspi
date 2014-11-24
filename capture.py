c53e0c687760e42049bc004843fd62dda74a8763
root <root@raspberrypi.(none)>
2014-11-24 20:28:02 +0000

    modified capture.py

--- capture.py
+++ capture.py
@@ -3,21 +3,21 @@ import picamera
 from fractions import Fraction
 
 with picamera.PiCamera() as camera:
-    #camera.resolution = (1024, 768)
+    camera.resolution = (1024, 768)
     #camera.framerate = 30 # ??
 
     #camera.iso = 1200
-    time.sleep(2)
+    #time.sleep(2)
 
     #camera.shutter_speed = camera.exposure_speed
     camera.framerate = Fraction(1,6)
     camera.shutter_speed = 6000000
     camera.exposure_mode = 'off'
-
+    camera.iso = 800
     #g = camera.awb_gains
     #camera.awb_mode = 'off'
     #camera.awb_gains = g
 
-    #time.sleep(10)
+    time.sleep(10)
 
     camera.capture('capture.jpg')
