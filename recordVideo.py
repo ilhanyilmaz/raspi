c33796023b281558f46fd212daff8c349b008ea2
root <root@raspberrypi.(none)>
2014-11-25 20:44:51 +0000

    modified:   recordVideo.py 	modified:   timelapse.py

--- recordVideo.py
+++ recordVideo.py
@@ -2,6 +2,6 @@ import picamera
 
 with picamera.PiCamera() as camera:
     camera.resolution = (640, 480)
-    camera.start_recording('my_video.avi')
-    camera.wait_recording(60)
+    camera.start_recording('my_video.h264')
+    camera.wait_recording(10)
     camera.stop_recording()
